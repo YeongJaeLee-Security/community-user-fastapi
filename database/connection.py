eba726ecf3fdd9049ad836a48a9c412dc4c46874
YeongJaeLee-Security <dudwo8528@gmail.com>
2024-11-26 17:54:24 +0900

    DataBase 생성 코드 수정

--- database/connection.py
+++ database/connection.py
@@ -1,7 +1,6 @@
 from config.Settings import Settings
 from sqlmodel import SQLModel, create_engine, Session
-from sqlalchemy.exc import OperationalError
-from sqlalchemy import text
+from sqlalchemy_utils import create_database, database_exists
 
 settings = Settings()
 
@@ -14,21 +13,15 @@ DATABASE_NAME = settings.DATABASE_NAME
 # 기본 연결 엔진 (DB 이름 없이 접속)
 base_engine = create_engine(BASE_DATABASE_URL, echo=True)
 
+# 실제 데이터베이스 연결 URL
+engine_url = f"{settings.DATABASE_URL}/{settings.DATABASE_NAME}"
+
 # 실제 데이터베이스 연결 엔진
-engine_url = create_engine(f"{settings.DATABASE_URL}/{settings.DATABASE_NAME}", echo=True)
+engine = create_engine(engine_url, echo=True)
 
 def create_database_if_not_exists():
-    """
-    데이터베이스가 없으면 생성하는 함수
-    """
-    try:
-        with base_engine.connect() as connection:
-            connection.execute(text(f"CREATE DATABASE IF NOT EXISTS {DATABASE_NAME};"))
-            connection.commit()  # 트랜잭션 커밋
-        print(f"Database '{DATABASE_NAME}' checked or created.")
-    except OperationalError as e:
-        print("Failed to connect to the database server:", e)
-        raise
+    if not database_exists(engine_url):  # URL 문자열 전달
+        create_database(engine_url)     # URL 문자열 전달
 
 def connection():
     """
@@ -38,7 +31,7 @@ def connection():
     create_database_if_not_exists()
 
     # SQLModel을 상속받은 모든 클래스를 기반으로 데이터베이스에 테이블 생성
-    SQLModel.metadata.create_all(engine_url)
+    SQLModel.metadata.create_all(engine)
 
 def get_session():
     """
@@ -46,5 +39,5 @@ def get_session():
     """
     # 데이터베이스와 상호작용(CRUD)를 관리하는 객체
     # with 구문으로 자원 해제 가능
-    with Session(engine_url) as session:
+    with Session(engine) as session:
         yield session
